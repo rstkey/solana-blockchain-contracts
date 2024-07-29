a969c6a85d335253c28330ab5184e101e81ef414
rust-solman <lussi.petelius@gmail.com>
2024-07-29 15:32:25 +0200

    devnet deploy

--- apps/initialize/src/main.rs
+++ apps/initialize/src/main.rs
@@ -8,14 +8,14 @@ use anchor_lang::system_program;
 use counter::{accounts, instruction, Counter, ID};
 
 const CLIENT_KEY_STR: &str =
-    "2cbenpqNMAMRmBmHX4RkYAyM6wRehs19tzY9HEA5A6QY7QJz7KhVhuGS2PchZPsjZfhN7wkwGu96JBpheiVx1WPM";
+    "4puAwQDQMsgv1466GctVMcQMAKxZ9qKN6tkB4dWWryqaoM5MbebA8wHC5qQn4VFTNKrSDNExX5LzzEsWr72zL3go";
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Invoking counter contract!");
     println!("program-id: {}", counter::ID);
     let payer = read_keypair_file(&*shellexpand::tilde("~/.config/solana/id.json"))
         .expect("failed to get the keypair");
-    let client = Client::new(Cluster::Localnet, Rc::new(payer));
+    let client = Client::new(Cluster::Devnet, Rc::new(payer));
 
     let program = client.program(counter::ID)?;
 
--- programs/counter/src/lib.rs
+++ programs/counter/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("AcfG5w5GjU95Ho6imbra9xBW5gvM5ujNdKN7PjNvtkfe");
+declare_id!("3Yij5WoKQ5fhRquLcrYTNhVZdgdY5xpaaTyygYvMWnTa");
 
 #[program]
 pub mod counter_anchor {
